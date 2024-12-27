75690be9c94c9c44488bff05ba2e805d21182b1c
Yonatan Isakov <yonatan156354@gmail.com>
2024-12-27 23:51:48 +0200

    edit app.ts to typescript

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,25 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import postRoutes from './routes/posts_routes';
+import comments from './routes/comments_routes';
+
 const app = express();
-const dotenv= require("dotenv").config();
+
+dotenv.config();
 const port = process.env.PORT;
 
-const mongoose =require("mongoose");
 mongoose.connect(process.env.DB_CONNECT);
+const db = mongoose.connection;
+db.on("error", (error) => { console.error(error) });
+db.once("open", () => console.log("Connected to Mongoose"));
 
-const db= mongoose.connection;
-db.on("error",(error)=>{console.error(error)});
-db.once("open",()=>console.log("Connected to Mongoose"));
-
-const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const postRoutes =require("./routes/posts_routes");
-app.use("/posts",postRoutes);
-
-const comments =require("./routes/comments_routes");
-app.use("/comments",comments);
+app.use("/posts", postRoutes);
+app.use("/comments", comments);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
