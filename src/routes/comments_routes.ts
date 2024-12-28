9ac5af56a13471cfbe34ef2ccbaf16d9a9668951
Yonatan Isakov <yonatan156354@gmail.com>
2024-12-28 21:55:15 +0200

    AUTH and Posts swagger added

--- src/routes/comments_routes.ts
+++ src/routes/comments_routes.ts
@@ -1,17 +1,17 @@
 import express from 'express';
-const router=express.Router();
+const router = express.Router();
 import commentsController from '../controllers/comments_controller';
-import {authTestMiddleware} from '../controllers/auth_controller';
+import { authTestMiddleware } from '../controllers/auth_controller';
 
 
-router.get("/",commentsController.getAll.bind(commentsController));
+router.get("/", commentsController.getAll.bind(commentsController));
 
-router.get("/:id",commentsController.getById.bind(commentsController));
+router.get("/:id", commentsController.getById.bind(commentsController));
 
-router.post("/", authTestMiddleware,commentsController.createItem.bind(commentsController));
+router.post("/", authTestMiddleware, commentsController.createItem.bind(commentsController));
 
-router.put("/:id", authTestMiddleware,commentsController.updateItemById.bind(commentsController));
+router.put("/:id", authTestMiddleware, commentsController.updateItemById.bind(commentsController));
 
-router.delete("/:id", authTestMiddleware,commentsController.deleteItem.bind(commentsController));
+router.delete("/:id", authTestMiddleware, commentsController.deleteItem.bind(commentsController));
 
 export default router;
